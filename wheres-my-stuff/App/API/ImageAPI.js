b26a9dc7fa0be97e356e6a475245a344571e0e8a
Angat77 <angat.yogi@ou.edu>
2024-03-15 17:15:48 -0500

    changed heroku to azure

--- wheres-my-stuff/App/API/ImageAPI.js
+++ wheres-my-stuff/App/API/ImageAPI.js
@@ -1,14 +1,14 @@
 const URL =
 "https://api-us-west-2.hygraph.com/v2/clspbrpww0ftj01w713oecr6o/master/upload";
 const HYGRAPH_ASSET_TOKEN  =`eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE3MDgyMDkyMjAsImF1ZCI6WyJodHRwczovL2FwaS11cy13ZXN0LTIuaHlncmFwaC5jb20vdjIvY2xzcGJycHd3MGZ0ajAxdzcxM29lY3I2by9tYXN0ZXIiLCJtYW5hZ2VtZW50LW5leHQuZ3JhcGhjbXMuY29tIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC11cy13ZXN0LTIuaHlncmFwaC5jb20vIiwic3ViIjoiZjY5YTk2NjgtNmJkZC00NzRiLWJiNzAtODYyMzA4NzUwNWE1IiwianRpIjoiY2xzcW5ueTExMGFldTAxbjVnZzJ0Mm44MyJ9.kYgGs1UpRYp0PwJ4_dic6JIZXnr8TxHLKjFilz78OC2eiiBhbwPR6OUzPjRq4v7WSSjrqzVBYSuOSbFIIr9XSAhnky3CE04CeakL79BPovHEi8UedDWXKxcnkKVLryJLkfAAPxLOypBzeWZC9m-Brw6DLRXLS4sxjr1nLxjGfggN4X0_WxT7o3t0F3GVhUZKnsFKGr-S-flOtrgGBmXVJnNRuv2bsICBB0YB4HCvC0zdz6eTeFpSpCxL8ujVTc5spA5o-U-2zIGfUUXrSAhgciiwCT1_EIjcI5jxXWrhvup2nPW8fbnW64-bBgeADdz8S6y88P71vtbTcFna-V4EYqgFRjIOtYtvkzRH4MMq6q6FR6xOiiRDFu8ty5kinqrAHwKZdsIICtgVbJWDRqHOiCHEJxwV7EChGZaPER5aSBUzOkRgBXyL0fFW4U-hbeAMqptsL3t_GEaKj8Eg98p9qHzVpeIw3P2xgELtCB32PAjrlmNObvBt0gbPPXNE68yirWFbO71_pb_KAyZk_bfC_NurP21-HSvsDch7yDiK859AVsARkw8CzxpJusNpiM8H4ymaVH71WfJ2-Dg8H09a3TuPYsTk_bZTaI3om4ZkFNvOUIYVcAJGHRRr1SnxMI3gumx3DSObEVfoloMbPVqrSBjOoO00sn-crg_pwAHCTFc`;
-const IMAGE_URL='https://wms-image-bg-remover-api-a8fc745d4eab.herokuapp.com/get-finalimage'
-
+const AZURE_URL='https://wms-image-bgrm-api.azurewebsites.net/get-finalimage'
 async function getFormattedImage(formData) {
+  console.log("image api format called")
   if(!formData){
     return;
   }
     try {
-      const response = await fetch(IMAGE_URL, {
+      const response = await fetch(AZURE_URL, {
         method: 'POST',
         body: formData,
         headers: {
@@ -19,6 +19,7 @@ async function getFormattedImage(formData) {
       throw new Error('Failed to process image');
     }
 
+    console.log("response ",response)
     const contentType = response.headers.get('content-type');
 
     if (!contentType.startsWith('image/')) {
